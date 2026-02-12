9006ad9f138a3cd05fc75f62c7a1a772ad0d839b
Akshay H <akshayhuliyaru2005@gmail.com>
2026-02-12 22:25:12 +0530

    New project added

---
+++ dice_play.py
@@ -0,0 +1,11 @@
+#The new project-dice-roll using random module
+import random 
+random_num=random.randint(1,6)
+user_input=input("Are you interested in playing dice roll game?(y/n)  ").lower()
+if user_input=='y':
+    print("Welcome to dice rolling game.")
+    dice_roll=random_num
+    print(f'The rolled dice number is {dice_roll}')
+else:
+    print("oh! Try next time.")
+    
